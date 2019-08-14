462d39d4fa37ea20c74d7f018519aebdd8db64a5
UviniC <uviniwijesinghe@gmail.com>
2019-08-14 10:30:24 +0530

    completed

--- app/src/main/java/com/example/fragmentlab/MainActivity.java
+++ app/src/main/java/com/example/fragmentlab/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.fragmentlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeFragment(View view){
+        Fragment fragment;
+
+        if(view == findViewById(R.id.btnFragment1)){
+            fragment = new fragment1();
+
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragmentDefault,fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.btnFragment2)){
+            fragment = new fragment2();
+
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragmentDefault,fragment);
+            ft.commit();
+        }
     }
 }
